216f356b1bc46edb8bfecac7e24186dfe5f71766
Deepak Bandla <deepakbandla4567@gmail.com>
2026-03-17 22:48:39 +0530

    Removed unnecessary files

--- src/evaluate.py
+++
@@ -1,8 +0,0 @@
-from sklearn.metrics import precision_score, recall_score
-
-
-def evaluate(model, X_test, y_test):
-    preds = model.predict(X_test)
-
-    print("Precision:", precision_score(y_test, preds))
-    print("Recall:", recall_score(y_test, preds))
